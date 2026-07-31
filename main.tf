1f449a3a49d615d26946731bfe511527a004a8c8
danielaraujoinbox-ship-it <daniel.araujo.inbox@gmail.com>
2026-07-30 23:32:43 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,6 @@ resource "aws_security_group" "web-sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/24"]
   }
 }
